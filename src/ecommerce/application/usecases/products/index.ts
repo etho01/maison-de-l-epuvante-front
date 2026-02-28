c73113142647f5830dbc4933a92846e7f1716156
Nicolas <barbeynicolas.basly@gmail.com>
2026-02-28 11:10:44 +0100

    fix(ecommerce/usecases): export GetProductBySlugUseCase from products index
    Le use case existait mais n'était pas exporté dans l'index barrel,
    le rendant inaccessible via le container ecommerce

--- src/ecommerce/application/usecases/products/index.ts
+++ src/ecommerce/application/usecases/products/index.ts
@@ -1,5 +1,6 @@
 export { GetProductsUseCase } from './GetProductsUseCase';
 export { GetProductByIdUseCase } from './GetProductByIdUseCase';
+export { GetProductBySlugUseCase } from './GetProductBySlugUseCase';
 export { CreateProductUseCase } from './CreateProductUseCase';
 export { UpdateProductUseCase } from './UpdateProductUseCase';
 export { DeleteProductUseCase } from './DeleteProductUseCase';
