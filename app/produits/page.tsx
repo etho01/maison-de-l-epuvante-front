7a358388fe702708a66c1225145de173b2b9869c
nicolas <barbeynicolas.basly@gmail.com>
2026-04-29 10:46:23 +0200

    update

--- app/produits/page.tsx
+++ app/produits/page.tsx
@@ -24,7 +24,7 @@ export default async function ProduitsPage() {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-8">Nos Produits</h1>
+      <h1 className="text-3xl font-bold mb-8">Nos Produits pour vous faire peur</h1>
       <ProductCatalog 
         initialProducts={products} 
         initialProductPagination={pagination} 
